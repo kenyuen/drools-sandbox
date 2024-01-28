024def6fee61cb95ae30d1097259a5e82722c01e
Ken Yuen <kenneth.x.yuen@gmail.com>
2024-01-28 13:58:29 -0600

    Updates to allow application to run

--- src/main/java/org/yuenio/droolSandbox/DroolSandboxApplication.java
+++ src/main/java/org/yuenio/droolSandbox/DroolSandboxApplication.java
@@ -1,4 +1,4 @@
-package org.yuenio.droolSandboxApplication;
+package org.yuenio.droolSandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
