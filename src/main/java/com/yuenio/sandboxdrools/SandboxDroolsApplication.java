cec365f487aee1a7df7a8eb27c26409891a102d4
Ken Yuen <kenneth.x.yuen@gmail.com>
2024-01-28 09:27:52 -0600

    Scaffold initialization

---
+++ src/main/java/com/yuenio/sandboxdrools/SandboxDroolsApplication.java
@@ -0,0 +1,13 @@
+package com.yuenio.sandboxdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxDroolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuenio/sandboxdrools/SandboxDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuenio.sandboxdrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
